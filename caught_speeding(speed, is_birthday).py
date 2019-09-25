67739f1e52dd71c5c4a9fcff8dc5556782767822
Akshat Zala <50409080+akshatz123@users.noreply.github.com>
2019-09-25 14:04:28 +0530

    Add files via upload

---
+++ caught_speeding(speed, is_birthday).py
@@ -0,0 +1,8 @@
+def caught_speeding(speed, is_birthday):
+  if speed <= 60:
+    return 0
+  elif speed <= 80 or speed >= 61:
+    return 1
+  elif speed > 80:
+    return 2
+  else: return speed * 5
